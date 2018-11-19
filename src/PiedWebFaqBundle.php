a9ca9ed7adb42bbe2927aa2a9ed777bff720c5c0
Robin <contact@robin-d.fr>
2018-11-19 21:04:13 +0100

    add dependecy injection

--- src/PiedWebFaqBundle.php
+++ src/PiedWebFaqBundle.php
@@ -3,7 +3,16 @@
 namespace PiedWeb\FaqBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
+use PiedWeb\FaqBundle\DependencyInjection\PiedWebFaqExtension;
 
 class PiedWebFaqBundle extends Bundle
 {
+    public function getContainerExtension()
+    {
+        if (null === $this->extension) {
+            $this->extension = new PiedWebFaqExtension();
+        }
+
+        return $this->extension;
+    }
 }
